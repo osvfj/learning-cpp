c43695c2a0e4cb9e19d16ac77bb5ffe18d40d647
osvfj <osvfj@duck.com>
2025-02-15 22:32:18 -0400

    add deposit and withdrawal functionality

--- quinta-tarea/banco.cpp
+++ quinta-tarea/banco.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main() {
   string dueño = "";
   float cantidadTotal = 0.00;
+  float deposito = 0.00;
+  float retiro = 0.00;
+
   bool seguir = false;
+  int opt;
 
   cout << "Digite su nombre: ";
   getline(cin, dueño);
@@ -31,7 +35,35 @@ int main() {
     }
   } while (!seguir);
 
-  cout << "Continue...";
+  cout << "¿Tansacción? \n";
+  cout << "1. Depósito\n2. Retiro\n";
+  cout << "Opción: ";
+  cin >> opt;
+
+  switch (opt) {
+    case 1:
+      cout << "Digite depósito: ";
+      cin >> deposito;
+
+      cantidadTotal += deposito;
+
+      cout << cantidadTotal << endl;
+      break;
+    case 2:
+      while (true) {
+        cout << "Digite retiro: ";
+        cin >> retiro;
+        if (retiro > cantidadTotal) {
+          cout << "No cuenta con los fondos necesarios para retirar esa "
+                  "cantidad \n";
+        } else {
+          cantidadTotal -= retiro;
+          break;
+        }
+      }
+    default:
+      break;
+  }
 
   return 0;
 }
