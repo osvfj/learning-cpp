09957087fc231b3d83b1d8949e6e53f57de6b74b
osvfj <osvfj@duck.com>
2025-02-15 22:43:08 -0400

    add infinite loop

--- quinta-tarea/banco.cpp
+++ quinta-tarea/banco.cpp
@@ -18,51 +18,53 @@ int main() {
   cout << "Digite monto de apertura: ";
   cin >> cantidadTotal;
 
-  do {
-    int continuar;
-    cout << "¿Desea continuar? (1. Si/2. No) ";
-    cin >> continuar;
+  while (true) {
+    do {
+      int continuar;
+      cout << "¿Desea continuar? (1. Si/2. No) ";
+      cin >> continuar;
 
-    switch (continuar) {
+      switch (continuar) {
+        case 1:
+          seguir = true;
+          break;
+        case 2:
+          exit(0);
+        default:
+          continue;
+          break;
+      }
+    } while (!seguir);
+
+    cout << "¿Tansacción? \n";
+    cout << "1. Depósito\n2. Retiro\n";
+    cout << "Opción: ";
+    cin >> opt;
+
+    switch (opt) {
       case 1:
-        seguir = true;
+        cout << "Digite depósito: ";
+        cin >> deposito;
+
+        cantidadTotal += deposito;
+
+        cout << cantidadTotal << endl;
         break;
       case 2:
-        exit(0);
+        while (true) {
+          cout << "Digite retiro: ";
+          cin >> retiro;
+          if (retiro > cantidadTotal) {
+            cout << "No cuenta con los fondos necesarios para retirar esa "
+                    "cantidad \n";
+          } else {
+            cantidadTotal -= retiro;
+            break;
+          }
+        }
       default:
-        continue;
         break;
     }
-  } while (!seguir);
-
-  cout << "¿Tansacción? \n";
-  cout << "1. Depósito\n2. Retiro\n";
-  cout << "Opción: ";
-  cin >> opt;
-
-  switch (opt) {
-    case 1:
-      cout << "Digite depósito: ";
-      cin >> deposito;
-
-      cantidadTotal += deposito;
-
-      cout << cantidadTotal << endl;
-      break;
-    case 2:
-      while (true) {
-        cout << "Digite retiro: ";
-        cin >> retiro;
-        if (retiro > cantidadTotal) {
-          cout << "No cuenta con los fondos necesarios para retirar esa "
-                  "cantidad \n";
-        } else {
-          cantidadTotal -= retiro;
-          break;
-        }
-      }
-    default:
-      break;
   }
 
   return 0;
