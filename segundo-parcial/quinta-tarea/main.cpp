388260ad2788978c9bf6e19f3c0f24116cc09450
osvfj <osvfj@duck.com>
2025-04-06 23:06:16 -0400

    add new employee information

--- segundo-parcial/quinta-tarea/main.cpp
+++ segundo-parcial/quinta-tarea/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <string>
 
-
 using namespace std;
+
+const int NUMERO_DE_EMPLEADOS = 10;
 
 struct Empleado {
     int id;
     string nombre;
     int horas_trabajadas;
     int precio_por_hora;
-}empleados[10];
+}empleados[NUMERO_DE_EMPLEADOS];
+
+
+void agregarEmpleado(){
+    for(int i = 0; i < NUMERO_DE_EMPLEADOS; i++){
+        if(!empleados[i].id){
+            empleados[i].id = i;
+            cout << "Nombre del empleado: ";
+            cin >> empleados[i].nombre;
+            cout << "Horas trabajadas: ";
+            cin >> empleados[i].horas_trabajadas;
+            cout << "Precio de la hora: ";
+            cin >> empleados[i].precio_por_hora;
+            break;
+        }
+    }
+}
+
 
 int main(){
 
@@ -23,8 +41,10 @@ int main(){
         cin >> opt;
 
 
+
         switch (opt){
-            
+            case 1:
+                agregarEmpleado();
         }
     }
 
