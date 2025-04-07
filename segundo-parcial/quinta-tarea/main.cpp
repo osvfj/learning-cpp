f09af2213ec5f89c985dd3503e4668665e228ab2
osvfj <osvfj@duck.com>
2025-04-06 22:25:57 -0400

    create struct

---
+++ segundo-parcial/quinta-tarea/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+struct Empleado {
+    int id;
+    string nombre;
+    int horas_trabajadas;
+    int precio_por_hora;
+}empleados[10];
+
+int main(){
+
+
+    return 0;
+}
